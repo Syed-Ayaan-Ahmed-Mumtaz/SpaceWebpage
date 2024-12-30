4b416f4379b027ebac51d2b7162b4918eded2967
nabiraa <nabiraasalman@gmail.com>
2024-12-30 22:43:01 +0500

    Update main.js
    added "show scroll up", "scroll sections active link", "scroll reveal animation"

--- Space Webpage/assets/js/main.js
+++ Space Webpage/assets/js/main.js
@@ -58,10 +58,52 @@ const swiperTravel = new Swiper('.travel swiper', {
   });
   
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+     //when the scroll os higher than 350 viewport height, add the show-scroll class to the tag with the scrollup class
+     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2500,
+    delay: 300,
+    // reset: true, // Animations repeat
+})
 
+sr.reveal(`.home__data, .travel__swiper, .contact__container`)
+sr.reveal(`.home__img`, {origin: 'bottom'})
+sr.reveal(`.home__ovni`, {delay: 800})
+sr.reveal(`.explore__img`, {origin: 'left'})
+sr.reveal(`.explore__data`, {origin: 'right'})
+sr.reveal(`.explore__planet`, {origin: 'right', delay: 800})
+sr.reveal(`.history__card`, {interval: 100})
+sr.reveal(`.history__planet-1`, {origin: 'left', delay: 1000})
+sr.reveal(`.history__planet-2`, {origin: 'right', delay: 1200})
+sr.reveal(`.footer__planet-1`, {origin: 'bottom', delay: 600})
+sr.reveal(`.footer__planet-2`, {delay: 800})
